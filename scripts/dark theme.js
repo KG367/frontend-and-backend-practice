6d6a13a60a5f769cc2f9ab465f818703f47c2bee
KG367 <anchouskostya@gmail.com>
2025-10-18 23:24:28 +0300

    Передумал, вот 11 тетрадка

---
+++ scripts/dark theme.js
@@ -0,0 +1,23 @@
+const toggleSwitch = document.getElementById('checkbox');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+}
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
